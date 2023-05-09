ecb179267be7d43c07668d39cd3a00ec7dbc2f4e
SKAR <125751744+SkarlethCM@users.noreply.github.com>
2023-05-08 22:08:46 -0500

    Add files via upload

---
+++ CastroMurilloSkarlethPromediomayor.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+float x,pm=0;
+int i=0,l;
+cout<<"Ingrese l : ";
+cin>>l;
+do {                                               
+	cout<<"Ingrese x: "; 
+	cin>>x;
+i=i+1;
+
+if (x>pm){
+        pm=x;
+}
+
+}while (i<l);
+cout<<" El promedio maximo de el curso fue : "<<pm<<endl;
+
+
+
+
+return 0;                                                          
+}
